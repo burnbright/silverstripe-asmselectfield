1371669ce73c6ba15b3ef6a96aa019396962f9e9
Jeremy Shipman <jeremy@burnbright.net>
2010-05-04 05:54:57 +0000

    asmselect field now loads from db

--- code/AsmselectField.php
+++ code/AsmselectField.php
@@ -6,6 +6,12 @@
  * http://code.google.com/p/jquery-asmselect/
  * 
  * @author Jeremy Shipman http://www.burnbright.co.nz
+ * 
+ * Example usage:
+ * 
+ * $members = DataObject::get('ID','Title');
+ * $asmfield = new AsmselectField('SpecialMembers','Special Members',$members);
+ * 
  */
 
 class AsmselectField extends DropdownField{
@@ -13,16 +19,20 @@ class AsmselectField extends DropdownField{
 	protected $rollbacksize = 10; //size of multiple select filed if js is broken
 	protected $usejavascript = true;
 	
+	public $dontEscape = true;
+	protected $reserveNL = false;
+	
 	function __construct($name, $title = null, $source = array(), $values = array(), $form = null) {
 		parent::__construct($name, $title, $source, $values , $form, null);
 	}
 	
 	function Field() {
-		
+
 		if($this->usejavascript){
 			//Requirements::css('jqueryfields/asmselect/jquery.asmselect.css'); //can be included seperately if desired
 			Requirements::javascript('jsparty/jquery/jquery.js');
-			Requirements::javascript('jqueryfields/asmselect/jquery.asmselect.js');
+			Requirements::javascript('jsparty/jquery/livequery/jquery.livequery.js');
+			Requirements::javascript('asmselectfield/javascript/jquery.asmselect.js');
 			
 			$id = $this->id();
 			$removelabel = 'remove'; //TODO: allow to be custom
@@ -30,10 +40,12 @@ class AsmselectField extends DropdownField{
 			//TODO: provide more customisation options
 			$script = <<<JS
 					jQuery(document).ready(function($) {
-					    $("select#$id").asmSelect({
-					    	listType: 'ul',
-					    	removeLabel: "$removelabel"
-					    });
+					    $("select#$id").livequery(function(){
+						    $(this).asmSelect({
+						    	listType: 'ul',
+						    	removeLabel: "$removelabel"
+						    });
+						});
 					}); 
 JS;
 			Requirements::customScript($script,'asmselect'.$this->name);
@@ -42,6 +54,22 @@ JS;
 		$options = '';
 		
 		$source = $this->getSource();
+		$values = $this->value;
+		
+		// Get values from the join, if available
+		if(is_object($this->form)) {
+			$record = $this->form->getRecord();
+			if(!$values && $record && $record->hasMethod($this->name)) {
+				$funcName = $this->name;
+				$join = $record->$funcName();
+				if($join) {
+					foreach($join as $joinItem) {
+						$values[] = $joinItem->ID;
+					}
+				}
+			}
+		}
+		
 		if($source) {
 			// For SQLMap sources, the empty string needs to be added specially
 			if(is_object($source) && $this->emptyString) {
@@ -51,13 +79,13 @@ JS;
 			foreach($source as $value => $title) {
 				
 				// Blank value of field and source (e.g. "" => "(Any)")
-				if($value === '' && ($this->value === '' || $this->value === null)) {
+				if($value === '' && ($values === '' || $values === null)) {
 					$selected = 'selected';
 				} else {
 					// Normal value from the source
 					$selected = null;					
-					if(count($this->value) > 0){
-						$selected = (in_array($value,$this->value)) ? 'selected' : null;
+					if(count($values) > 0){
+						$selected = (in_array($value,$values)) ? 'selected' : null;
 						$this->isSelected = ($selected) ? true : false;
 					}
 				}
@@ -92,7 +120,10 @@ JS;
 		$saveDest = $record->$fieldName();
 		if(! $saveDest)
 			user_error("AsmselectField::saveInto() Field '$fieldName' not found on $record->class.$record->ID", E_USER_ERROR);
-		$saveDest->setByIDList($this->value);
+		
+		$values = explode(",",$this->value[0]);
+		$saveDest->setByIDList($values);
+
 	}
 	
 	function setListSize($size){
